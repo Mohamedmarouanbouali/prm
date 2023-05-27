32b4ef07bc9b0d6331ba6ae8b82c46e6eaf96d9e
Marouan-bouali <122131776+Marouan-bouali@users.noreply.github.com>
2023-05-27 14:32:07 +0200

    create tqble commad

---
+++ WebECommerce/src/main/java/com/web/model/commande.java
@@ -0,0 +1,17 @@
+package com.web.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+@Entity
+@Table(name="commande")
+
+
+public class commande {
+
+
+
+}
